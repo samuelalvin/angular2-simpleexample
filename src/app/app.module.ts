26bef238e7a947a8c667e5fbf8a7bb94f87ca876
samuelalvin <samuel.alvin1994@gmail.com>
2017-04-06 17:55:26 +0700

    add userList view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent }  from "./app.component";
+import { UserListComponent }  from "./userList/user-list.component";
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ 
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   bootstrap: [
     AppComponent
---
+++ src/app/userList/user-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-user-list",
+  templateUrl: "/app/userList/user-list.html"
+})
+
+export class UserListComponent {
+    
+}
