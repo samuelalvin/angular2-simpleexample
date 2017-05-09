3a5b034d9519f76ce9662a493cc06c56eb18f596
samuelalvin <samuel.alvin1994@gmail.com>
2017-05-09 19:51:07 +0700

    add performance test

--- src/app/mockData/user/index.ts
+++ src/app/mockData/user/index.ts
@@ -2,18 +2,503 @@ import { IUser } from "../../interface/user";
 
 export const mockUsers: IUser[] = [
     {
+        id: 0,
+        name: "sampleUser",
+        age: 0
+    },
+    {
         id: 1,
-        name: "Aaron",
-        age: 20
+        name: "sampleUser",
+        age: 0
     },
     {
         id: 2,
-        name: "Barry",
-        age: 23
+        name: "sampleUser",
+        age: 0
     },
     {
         id: 3,
-        name: "Cecil",
-        age: 19
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 4,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 5,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 6,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 7,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 8,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 9,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 10,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 11,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 12,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 13,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 14,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 15,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 16,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 17,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 18,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 19,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 20,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 21,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 22,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 23,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 24,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 25,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 26,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 27,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 28,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 29,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 30,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 31,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 32,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 33,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 34,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 35,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 36,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 37,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 38,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 39,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 40,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 41,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 42,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 43,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 44,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 45,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 46,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 47,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 48,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 49,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 50,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 51,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 52,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 53,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 54,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 55,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 56,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 57,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 58,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 59,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 60,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 61,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 62,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 63,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 64,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 65,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 66,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 67,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 68,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 69,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 70,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 71,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 72,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 73,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 74,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 75,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 76,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 77,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 78,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 79,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 80,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 81,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 82,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 83,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 84,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 85,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 86,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 87,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 88,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 89,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 90,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 91,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 92,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 93,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 94,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 95,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 96,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 97,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 98,
+        name: "sampleUser",
+        age: 0
+    },
+    {
+        id: 99,
+        name: "sampleUser",
+        age: 0
     }
 ];
--- src/app/userList/user-list.component.ts
+++ src/app/userList/user-list.component.ts
@@ -1,17 +1,33 @@
 import { IUser } from "../interface/user";
 import { mockUsers } from "../mockData/user";
 
-import { Component } from "@angular/core";
+import { Component, AfterViewChecked } from "@angular/core";
 
 @Component({
   selector: "my-user-list",
   templateUrl: "/app/userList/user-list.html"
 })
 
-export class UserListComponent {
-    private userList: IUser[]; 
-    
-    constructor() {
-      this.userList = mockUsers; 
+export class UserListComponent implements AfterViewChecked {
+    private userList: IUser[] = []; 
+    private startTime = 0;
+    private endTime = 0;
+
+    constructor() { }
+
+    ngAfterViewChecked() {
+        this.endTime = performance.now();
+        let timeSpent = this.endTime - this.startTime;
+        console.debug(timeSpent + "ms.");
+    }
+
+    addUsers() {
+        this.startTime = performance.now();
+        this.userList = mockUsers;
+    }
+
+    clearUsers() {
+        this.startTime = performance.now();
+        this.userList = [];
     }
 }
